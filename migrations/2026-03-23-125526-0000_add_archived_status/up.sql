7771f96c53093c21628c30d23224f59a8075d4fa
Félix Saparelli <felix@bes.au>
2026-03-24 01:56:00 +1300

    fix(db): add 'archived' to proposals_status_check constraint

---
+++ migrations/2026-03-23-125526-0000_add_archived_status/up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE proposals DROP CONSTRAINT proposals_status_check;
+ALTER TABLE proposals ADD CONSTRAINT proposals_status_check
+    CHECK (status IN ('drafting', 'finalising', 'submitted', 'in_progress', 'merged', 'abandoned', 'archived'));
